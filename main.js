a0a9bdc5a9ca7367e06ff467b4c48bab5c33a401
unknown <Kat2bk@gmail.com>
2020-04-06 18:54:08 -0700

    git did not save or recognize the last changes I made. I could not get the external js file to work, so moved all work inline html file. threw away onclick event on button, used window.onload instead. set an if statement to change the background color of ball when button clicked.

--- main.js
+++ main.js
@@ -1,22 +1,29 @@
-var answers =  ["It is certain",
-"Without a doubt!",
-"As I see it, yes",
-"Most likely",
-"All signs point to yes",
-"Don't count on it",
-"My answer is no",
-"Doubtful",
-"Outlook not good",
-"Ask again later",
-"Cannot predict now",
-"The future is hazy",
-]
+// var array = [
+//   "It is certain",
+//   "Without a doubt!",
+//   "As I see it, yes",
+//   "Most likely",
+//   "All signs point to yes",
+//   "Don't count on it",
+//   "My answer is no",
+//   "Doubtful",
+//   "Outlook not good",
+//   "Ask again later",
+//   "Cannot predict now",
+//   "The future is hazy",
+//   "Maybe so",
+//   "Unsure, try again",
+//   "Hahahahaha, really?",
+// ];
 
+// const question = document.getElementById("button");
+// let answer = document.getElementById("answer");
+// let eightText = document.getElementById("design");
 
-window.onload = function () {
-  const question = document.getElementById("button");
-
-  question.addEventListener("click", function() {
-
-  })
-};
+// var getAnswer = () => {
+//   question.addEventListener("click", function () {
+//     eightText.innerText = "";
+//     var item = array[Math.floor(Math.random() * array.length)];
+//     answer.innerText = array[item];
+//   });
+// };
